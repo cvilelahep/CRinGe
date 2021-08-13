81f3d7434d3f180221e01d8494584ebd8681cfbc
Junjie Xia <junjiex@cedar5.cedar.computecanada.ca>
2021-08-13 03:38:56 -0700

    setting random seed for reproducibility check (default seed = 0), adding more data loading flags to improve processing

--- iotools/loader_factory.py
+++ iotools/loader_factory.py
@@ -7,13 +7,16 @@ from torch.utils.data import DataLoader
 def loader_factory(name, batch_size,
                    shuffle=True,
                    num_workers=1,
+                   worker_init_fn=None,
                    collate_fn=iotools.hkdataset.Collate,
+                   pin_memory=True,
                    **args):
     ds = getattr(iotools.hkdataset,name)(**args)
     loader = DataLoader(ds,
                         batch_size  = batch_size,
                         shuffle     = shuffle,
                         num_workers = num_workers,
-                        collate_fn  = collate_fn)
+                        worker_init_fn = worker_init_fn,
+                        collate_fn  = collate_fn,
+                        pin_memory = pin_memory)
     return loader
-
